8253ab60f0b6f426c2df06c9dd6993ab8d56518e
Lucile Jones <jones.lucile@gmail.com>
2024-02-02 16:17:58 -0700

    finished crud functionality for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # get 'users/index'
   get '/users', to: 'users#index'
+
+  get '/users/:id', to: 'users#show'
+
+  post '/users', to: 'users#create'
+
+  delete '/users/:id', to: 'users#destroy'
 end
