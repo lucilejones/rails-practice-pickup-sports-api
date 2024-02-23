2da136bc106d00545293ce658dcaac3836b706a1
Lucile Jones <jones.lucile@gmail.com>
2024-02-23 10:53:47 -0700

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+(1..10).each do |i|
+    user = User.create(
+        username: Faker::Internet.username(specifier: 3..20, separators: %w(_)),
+        email: Faker::Internet.email,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        password: 'password',
+        password_confirmation: 'password'
+    )
+
+    rand(1..10).times do
+        user.posts.create(content: Faker::Lorem.paragraph)
+    end
+end
