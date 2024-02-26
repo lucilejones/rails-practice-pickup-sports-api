82b31f4b943b08521990fdde84954f35722b24de
Lucile Jones <jones.lucile@gmail.com>
2024-02-26 16:34:14 -0700

    added test for events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,7 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get '/users', to: 'users#index'
-
-  # get '/users/:id', to: 'users#show'
-
-  # post '/users', to: 'users#create'
-
-  # delete '/users/:id', to: 'users#destroy'
-
-
-  # resources :users
-
-  # get '/users/:id/posts', to: 'users#posts_index'
-
-
+  resources :events
+  resources :posts
   resources :users do
     get 'posts', to: 'users#posts_index'
   end
-
-  # resources :posts, only: [:create, :update, :destroy]
-  resources :posts
 end
